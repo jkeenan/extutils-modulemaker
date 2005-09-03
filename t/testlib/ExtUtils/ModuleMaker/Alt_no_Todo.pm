f579111e85816fe1f7b4d4021920765962b9106d
jimk <jimk@07862939-d7ef-0310-b512-86c215e7314a>
2005-09-03 23:44:07 +0000

    Added Alt_no_scripts_dir.pm and Alt_no_Todo.pm to version control.

---
+++ t/testlib/ExtUtils/ModuleMaker/Alt_no_Todo.pm
@@ -0,0 +1,17 @@
+package ExtUtils::ModuleMaker::Alt_no_Todo;
+use strict;
+local $^W = 1;
+
+=head3 C<text_Todo()>
+
+  Comment   : Subclass:  No content to Todo, which should mean to Todo file
+              will be generated
+
+=cut
+
+sub text_Todo {
+    my $self = shift;
+    return;
+}
+
+1;
